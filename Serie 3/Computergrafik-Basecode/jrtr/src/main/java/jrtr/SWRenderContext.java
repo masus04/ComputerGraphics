79917e4dab4e08bc89fe0c8b64ae02eef269ca68
masus04 <thomas_st@hotmail.com>
2014-10-27 15:14:10 +0100

    Working on 3.3

--- Serie 3/Computergrafik-Basecode/jrtr/src/main/java/jrtr/SWRenderContext.java
+++ Serie 3/Computergrafik-Basecode/jrtr/src/main/java/jrtr/SWRenderContext.java
@@ -2,6 +2,7 @@ package jrtr;
 
 import jrtr.RenderContext;
 
+import java.awt.Color;
 import java.awt.image.*;
 import java.util.ArrayList;
 
@@ -20,8 +21,9 @@ public class SWRenderContext implements RenderContext {
 
 	private SceneManagerInterface sceneManager;
 	private BufferedImage colorBuffer;
+	private ArrayList<Triangle> triangles;
 
-	private static Matrix4f c, p, m, d;
+	private static Matrix4f c, p, m, d, transformation;
 
 	public void setSceneManager(SceneManagerInterface sceneManager) {
 		this.sceneManager = sceneManager;
@@ -75,32 +77,133 @@ public class SWRenderContext implements RenderContext {
 	 * 3D objects.
 	 */
 	private void draw(RenderItem renderItem) {
-		ArrayList<Triangle> triangles = extractTriangles(renderItem);
-		
-		c = sceneManager.getCamera().getCameraMatrix();
+		extractTriangles(renderItem);
+		initTransformationMatrix(renderItem);
+
+		int task = 1;
+
+		if (task == 0) {						// put all vertices in triangles for transformation
+			drawVertices(renderItem);
+		} else if (task == 1)					// put vertices into triangles according to indices
+			drawTriangleVertices(triangles);
+		else if (task == 2)
+			drawTriangles(triangles);
+
+	}
+
+	private void drawVertices(RenderItem renderItem) {
+
+		float[] vertices = renderItem.getShape().getVertexData().getPositions();
+		ArrayList<Triangle> triangles = new ArrayList<Triangle>();
+
+		for (int i = 0; i < vertices.length - 8; i += 9) {
+			Vector4f p1 = new Vector4f(new Vector4f(vertices[i + 0], vertices[i + 1], vertices[i + 2], 1));
+			Vector4f p2 = new Vector4f(new Vector4f(vertices[i + 3], vertices[i + 4], vertices[i + 5], 1));
+			Vector4f p3 = new Vector4f(new Vector4f(vertices[i + 6], vertices[i + 7], vertices[i + 8], 1));
+
+			triangles.add(new Triangle(p1, p2, p3));
+		}
+
+		drawTriangleVertices(triangles);
+
+	}
+
+	private void drawTriangleVertices(ArrayList<Triangle> triangles) {
+		Vector4f p;
+
+		for (Triangle t : triangles) {
+
+			t.transform(transformation);
+
+			p = new Vector4f(t.getP1());
+			if (inBounds(p, p.w))
+				colorBuffer.setRGB((int) (p.x / p.w), (int) (p.y / p.w), Color.WHITE.getRGB());
+
+			p = new Vector4f(t.getP2());
+			if (inBounds(p, p.w))
+				colorBuffer.setRGB((int) (p.x / p.w), (int) (p.y / p.w), Color.WHITE.getRGB());
+
+			p = new Vector4f(t.getP3());
+			if (inBounds(p, p.w))
+				colorBuffer.setRGB((int) (p.x / p.w), (int) (p.y / p.w), Color.WHITE.getRGB());
+		}
+	}
+
+	private void drawTriangles(ArrayList<Triangle> triangles) {
+		float[] boundingBox;
+		Vector4f pixel;
+
+		for (Triangle t : triangles) {
+			if (t.isVisible()) {
+
+				boundingBox = t.getBoundingBox();
+
+				for (int i = (int) boundingBox[0]; i < (int) boundingBox[1]; i++)
+					for (int j = (int) boundingBox[2]; j < (int) boundingBox[3]; j++) {
+						// loop over all pixels inside the bounding box
+						pixel = new Vector4f(i, j, 0, 1);
+						if(t.isdrawn(pixel))
+							colorBuffer.setRGB((int)pixel.x, (int)pixel.y, t.color(pixel));
+
+					}
+			}
+		}
+	}
+
+	/**
+	 * calculates the transformation Matrix4f
+	 * 
+	 * @param renderItem
+	 */
+	private void initTransformationMatrix(RenderItem renderItem) {
+		m = renderItem.getShape().getTransformation();
+		c = new Matrix4f(sceneManager.getCamera().getCameraMatrix());
 		p = sceneManager.getFrustum().getProjectionMatrix();
+		d = calculateDMatrix();
+
+		Matrix4f matrix = new Matrix4f(m);
+		matrix.mul(c, m);
+		matrix.mul(p, matrix);
+		matrix.mul(d, matrix);
+
+		transformation = matrix;
+	}
+
+	private Matrix4f calculateDMatrix() {
+		Matrix4f matrix = new Matrix4f();
+
+		matrix.setRow(0, new Vector4f(colorBuffer.getWidth() / 2, 0, 0, colorBuffer.getWidth() / 2));
+		matrix.setRow(1, new Vector4f(0, -colorBuffer.getHeight() / 2, 0, colorBuffer.getHeight() / 2));
+		matrix.setRow(2, new Vector4f(0, 0, 0.5f, 0.5f));
+		matrix.setRow(3, new Vector4f(0, 0, 0, 1));
+
+		return matrix;
 	}
 
 	private ArrayList<Triangle> extractTriangles(RenderItem renderItem) {
 		Triangle triangle;
-		ArrayList<Triangle> triangles = new ArrayList<Triangle>();
-		
+		triangles = new ArrayList<Triangle>();
+
 		float[] positions = renderItem.getShape().getVertexData().getPositions();
 		int[] indices = renderItem.getShape().getVertexData().getIndices();
+		float[] colors = renderItem.getShape().getVertexData().getColors();
+		float[] normals = renderItem.getShape().getVertexData().getNormals();
 
-		
-		Vector4f p1, p2, p3;
-		for (int i = 0; i < indices.length; i += 9) {
-			p1 = new Vector4f(positions[indices[i + 0]], positions[indices[i + 1]], positions[indices[i + 2]], 1);
-			p2 = new Vector4f(positions[indices[i + 3]], positions[indices[i + 4]], positions[indices[i + 5]], 1);
-			p3 = new Vector4f(positions[indices[i + 6]], positions[indices[i + 7]], positions[indices[i + 8]], 1);
+		Vector4f p1, p2, p3; // TODO
+		for (int i = 0; i < indices.length; i += 3) {
+			p1 = new Vector4f(positions[indices[i + 0] * 3 + 0], positions[indices[i + 0] * 3 + 1],
+					positions[indices[i + 0] * 3 + 2], 1);
+			p2 = new Vector4f(positions[indices[i + 1] * 3 + 0], positions[indices[i + 1] * 3 + 1],
+					positions[indices[i + 1] * 3 + 2], 1);
+			p3 = new Vector4f(positions[indices[i + 2] * 3 + 0], positions[indices[i + 2] * 3 + 1],
+					positions[indices[i + 2] * 3 + 2], 1);
 
 			triangle = new Triangle(p1, p2, p3);
-			triangle.calculateEdgeFunction();
-			
+			//triangle.calculateEdgeFunction();
+
 			triangles.add(triangle);
 		}
-		
+
 		return triangles;
 	}
 
@@ -132,5 +235,12 @@ public class SWRenderContext implements RenderContext {
 
 	public VertexData makeVertexData(int n) {
 		return new SWVertexData(n);
+	}
+
+	private boolean inBounds(Vector4f p, float w) {
+		int width = colorBuffer.getWidth();
+		int height = colorBuffer.getHeight();
+
+		return (p.x / w) >= 0 && (p.x / w) < width && (p.y / w) < height && (p.y / w) >= 0;
 	}
 }
--- Serie 3/Computergrafik-Basecode/jrtr/src/main/java/jrtr/Triangle.java
+++ Serie 3/Computergrafik-Basecode/jrtr/src/main/java/jrtr/Triangle.java
@@ -1,54 +1,172 @@
 package jrtr;
 
 import javax.vecmath.Matrix3f;
+import javax.vecmath.Matrix4f;
+import javax.vecmath.SingularMatrixException;
 import javax.vecmath.Vector3f;
 import javax.vecmath.Vector4f;
 
 public class Triangle {
-	private static Vector4f p1;
-	private static Vector4f p2;
-	private static Vector4f p3;
+	private static Vector4f p1, p2, p3;
+
+	private boolean isTransformed = false;
 
 	private static Matrix3f edgeFunctionsMatrix;
-	
-	public Triangle(Vector4f p1, Vector4f p2, Vector4f p3){
+
+	public Triangle(Vector4f p1, Vector4f p2, Vector4f p3) {
 		Triangle.p1 = p1;
 		Triangle.p2 = p2;
 		Triangle.p3 = p3;
-		
+
 		calculateEdgeFunction();
 	}
-	
-	public Vector4f getP1(){
+
+	public Vector4f getP1() {
 		return p1;
 	}
-	
-	public Vector4f getP2(){
+
+	public Vector4f getP2() {
 		return p2;
 	}
-	
-	public Vector4f getP3(){
+
+	public Vector4f getP3() {
 		return p3;
 	}
 
-	public void calculateEdgeFunction() {
+	public Matrix3f calculateEdgeFunction() {
 		edgeFunctionsMatrix = new Matrix3f();
-		
-		edgeFunctionsMatrix.setRow(0, new Vector3f(p1.x, p1.y, p1.z));
-		edgeFunctionsMatrix.setRow(1, new Vector3f(p2.x, p2.y, p2.z));
-		edgeFunctionsMatrix.setRow(2, new Vector3f(p3.x, p3.y, p3.z));
-		
-		edgeFunctionsMatrix.invert();
+
+		edgeFunctionsMatrix.setRow(0, new Vector3f(p1.x, p1.y, p1.w));
+		edgeFunctionsMatrix.setRow(1, new Vector3f(p2.x, p2.y, p2.w));
+		edgeFunctionsMatrix.setRow(2, new Vector3f(p3.x, p3.y, p3.w));
+
+		try {
+			edgeFunctionsMatrix.invert();
+		} catch (SingularMatrixException e) {
+			return edgeFunctionsMatrix = null;
+		}
+
+		return edgeFunctionsMatrix;
 	}
-	
-	public boolean isInside(Vector4f point){
+
+	public boolean isInside(Vector4f point) {
 		Matrix3f vectorM = new Matrix3f();
 		Vector3f vector = new Vector3f();
 		vectorM.setColumn(0, new Vector3f(point.x, point.y, point.z));
-		
+
 		vectorM.mul(edgeFunctionsMatrix, vectorM);
 		vectorM.getColumn(0, vector);
-		
+
 		return (vector.x > 0 && vector.y > 0 && vector.z > 0);
+	}
+
+	/**
+	 * transforms all points with the parameter transformation matrix
+	 * 
+	 * @param transformation
+	 */
+	public void transform(Matrix4f transformation) {
+		if (!isTransformed) {
+
+			transformation.transform(p1);
+			transformation.transform(p2);
+			transformation.transform(p3);
+
+			isTransformed = true;
+		}
+	}
+
+	public boolean isTransformed() {
+		return isTransformed;
+	}
+
+	public double alphaFunction(Vector4f pixel) {
+		return edgeFunctions(pixel, 0);
+	}
+
+	public double betaFunction(Vector4f pixel) {
+		return edgeFunctions(pixel, 1);
+	}
+
+	public double gammaFunction(Vector4f pixel) {
+		return edgeFunctions(pixel, 2);
+	}
+
+	private double edgeFunctions(Vector4f pixel, int abg) {
+		float[] floats = new float[3];
+		edgeFunctionsMatrix.getRow(abg, floats);
+
+		return floats[0] * pixel.x / pixel.w + floats[1] * pixel.y / pixel.w + floats[2];
+	}
+
+	/**
+	 * calculates if a pixel is inside the triangle using the edgefunctions
+	 */
+	public boolean isdrawn(Vector4f pixel) {
+		if (edgeFunctionsMatrix == null)			// singular edgefunctionmatrix
+			return false;
+
+		if (edgeFunctionsMatrix.determinant() < 0)	// backface culling
+			return false;
+
+		if (!isVisible())
+			return false;
+			
+		return (alphaFunction(pixel) > 0 && betaFunction(pixel) > 0 && gammaFunction(pixel) > 0);
+	}
+
+	public boolean isVisible() {
+		return (p1.w > 0 && p2.w > 0 && p3.w > 0);
+	}
+	
+	/**
+	 * 
+	 * @return an array containing the left, right, upper and lower border of the bounding box.
+	 * 
+	 * [leftBorder, rightBorder, upperBorder, lowerBorder]
+	 */
+	public float[] getBoundingBox(){
+		float[] values = new float[4];
+		
+		values[0] = min(p1.x, p2.x, p3.x);
+		values[1] = max(p1.x, p2.x, p3.x);
+		values[2] = min(p1.y, p2.y, p3.y);
+		values[3] = max(p1.y, p2.y, p3.y);
+	
+		return values;
+	}
+	
+	
+
+	private static float min(float a, float b, float c) {
+	      if (b < a) {
+	          a = b;
+	      }
+	      if (c < a) {
+	          a = c;
+	      }
+	      return a;
+	  }
+	
+	private float max(float a, float b, float c) {
+		 if (b > a) {
+	          a = b;
+	      }
+	      if (c > a) {
+	          a = c;
+	      }
+	      
+	      return a;
+	}
+
+	
+	/**
+	 * 
+	 * @param pixel
+	 * @return calculates the color to the pixel inside the triangle
+	 */
+	public int color(Vector4f pixel) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
